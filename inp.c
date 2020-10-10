6b79dfdbd2b3e358f5e584203ea419ee67497059
Aaron C Gaudette <acg@aaronsee.media>
2020-10-10 21:27:59 +0000

    Compute mouse delta and position

--- inp.c
+++ inp.c
@@ -1,6 +1,9 @@
 #ifdef INP_KEYS
 #include "inp.h"
 
+static v2 mouse_last;
+static float dirty;
+
 void inp_update(GLFWwindow *win)
 {
 	for (size_t i = 0; i < inp_data.key.count; ++i) {
@@ -18,6 +21,24 @@ void inp_update(GLFWwindow *win)
 		s |= glfwGetMouseButton(win, handle) == GLFW_PRESS;
 		inp_data.but.states[handle] = s;
 	}
+
+	double mx, my;
+	glfwGetCursorPos(win, &mx, &my);
+
+	float x = mx;
+	float y = my * -1.f;
+
+	inp_data.mouse.pos.x = x;
+	inp_data.mouse.pos.y = y;
+
+	mouse_last = v2_lerp(inp_data.mouse.pos, mouse_last, dirty);
+	dirty = 1.f;
+
+	inp_data.mouse.delta.x = x - mouse_last.x;
+	inp_data.mouse.delta.y = y - mouse_last.y;
+
+	mouse_last.x = x;
+	mouse_last.y = y;
 }
 
 #endif
