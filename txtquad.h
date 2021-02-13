77c6ddf5a533bfb3e524bb725309ea3f60e5b3b3
Aaron C Gaudette <acg@aaronsee.media>
2021-02-13 17:22:06 +0000

    Rehaul interface
    - struct Settings => struct txt_cfg
    - struct Extent => struct extent
    - struct Frame => struct txt_frame
    	- last_t => t_prev
    	- last_i => i_prev (and moved under debug define)
    	- win_size => size
    	- (data => frame)
    - struct Share => struct share
    	- Refactor extra vars to union
    - struct Text => struct txt_buf
    	- char_count => count
    	- chars => quads
    	- (text => txt)
    - struct Char => struct txt_quad
    	- Replace transform fields with m4
    	- v => value
    	- col => color
    	- fx => _extra
    - Remove struct Block (no longer supported)
    - DEBUG => TXT_DEBUG
    - Comment

--- txtquad.h
+++ txtquad.h
@@ -7,12 +7,12 @@
 #define PIX_WIDTH (1.f / CHAR_WIDTH)
 #define LINE_HEIGHT (PIX_WIDTH + 1.f)
 
-struct Extent {
+struct extent {
 	u16 w;
 	u16 h;
 };
 
-struct Settings {
+struct txt_cfg {
 	const char *app_name;
 	const char *asset_path; // .spv and .pbm files are loaded from here
 	enum {
@@ -20,76 +20,59 @@ struct Settings {
 		, MODE_FULLSCREEN // Note: unsupported aspect ratios will panic
 		, MODE_BORDERLESS
 	} mode;
-	struct Extent win_size; // Ignored for MODE_BORDERLESS
+	struct extent win_size; // Ignored for MODE_BORDERLESS
 };
 
-struct Frame {
-	struct Extent win_size;
+/* Per-frame data */
+
+struct txt_frame {
 	size_t i;
-	size_t last_i;
 	float t;
-	float last_t;
+	float t_prev;
 	float dt;
-#ifdef DEBUG
+	struct extent size;
+#ifdef TXT_DEBUG
 	float acc;
+	size_t i_last;
 #endif
 };
 
-struct Share {
+struct txt_share {
 	m4 vp;
-	v2 screen;
-	float time;
-	float _slop;
-	v4 _extra;
+
+	union {
+		// Common use case, but entirely optional;
+		// note that you must fill these yourself!
+		struct {
+			v2 screen;
+			float time;
+			float _[5];
+		};
+
+		float _extra[8];
+	};
 };
 
-struct Char {
-	v3 pos;
-	v4 rot;
-	float scale; // ... of one char, regardless of CHAR_WIDTH
-	u8 v;
-	v4 col;
-	v2 fx;
-};
-
-enum Justify {
-	  JUST_LEFT
-	, JUST_CENTER
-	, JUST_RIGHT
-};
-
-struct Block {
-	const char *str;
-	size_t str_len;
-	v3 pos;
-	v4 rot;
-	float scale;
-	v2 piv;
-	v2 off;
-	enum Justify just;
-	v4 col;
-	float spacing;
-	size_t col_lim; // TODO: parse newline chars
-	u8 cursor;
-};
-
-struct Text {
-	struct Block blocks[MAX_BLCK];
-	size_t block_count;
-	struct Char chars[MAX_CHAR];
-	size_t char_count;
+struct txt_buf {
+	size_t count;
+	struct txt_quad {
+		u8  value;
+		m4  model;
+		v4  color;
+		v2 _extra;
+	} quads[MAX_QUAD];
 };
 
 /*
  * User callback; must be implemented by consumer (you)
  */
 __attribute__((weak))
-struct Share txtquad_update(struct Frame data, struct Text *text);
+struct txt_share txtquad_update(struct txt_frame, struct txt_buf*);
 
 /*
  * Public API
  */
-void txtquad_init(const struct Settings);
+void txtquad_init(const struct txt_cfg);
 void txtquad_start();
 
 #endif
