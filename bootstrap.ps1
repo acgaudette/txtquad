5b3d74d7b5b1928ff02d5d8e6bd7daca684b96fb
Aaron C Gaudette <acg@aaronsee.media>
2021-07-06 00:02:01 -0400

    Update bootstrap scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,6 +3,8 @@ $ErrorActionPreference = "Stop"
 $VK_SDK="C:\VulkanSDK\1.2.*"
 $GLFW_URL="https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.zip"
 $FONT_URL="https://github.com/acgaudette/kufont-ascii.git"
+$HEAD_URL="https://github.com/acgaudette/acg.git"
+ $ALG_URL="https://github.com/acgaudette/alg.git"
 
 mkdir -Force assets
 cd assets
@@ -21,6 +23,18 @@ mkdir -Force ext/src
 mkdir -Force ext/lib
 mkdir -Force ext/include
 cd ext
+
+cd include
+
+if (-Not (Test-Path "acg")) {
+	git clone $HEAD_URL
+}
+
+if (-Not (Test-Path "alg")) {
+	git clone $ALG_URL
+}
+
+cd ..
 
 if (-Not (Test-Path "lib/vulkan.lib")) {
 	New-Item -Path "lib/vulkan.lib" -ItemType SymbolicLink -Value "$VK_SDK/Lib/vulkan-1.lib"
